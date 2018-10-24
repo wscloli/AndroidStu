8dcde7f2f8558ff6eca6190ee9f4a57b9d178894
wscloli <2549271039@qq.com>
2018-10-24 17:50:16 +0800

    study

---
+++ android2/Data Storage/DatabaseTest/app/src/main/java/com/example/administrator/databasetest/LoginFragment.java
@@ -0,0 +1,20 @@
+package com.example.administrator.databasetest;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LoginFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //获取碎片布局
+        View view=inflater.inflate(R.layout.login_fragment, container,false);
+        return view;
+    }
+}
--- android2/Data Storage/DatabaseTest/app/src/main/java/com/example/administrator/databasetest/MainActivity.java
+++ android2/Data Storage/DatabaseTest/app/src/main/java/com/example/administrator/databasetest/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.administrator.databasetest;
 
+import android.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.support.v4.app.Fragment;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 private Button saveButton;
@@ -14,18 +20,49 @@ private CheckBox saveCheck;
 private EditText accountEdit;
 private EditText passEdit;
 private MyDatabaseHelper dbHelper;
+    LinearLayout linearLayout;
+
+    public MainActivity() {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+//定义菜单响应事件
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.login_item:
+                android.support.v4.app.FragmentManager fragmentManager=getSupportFragmentManager();
+                FragmentTransaction transaction=fragmentManager.beginTransaction();
+
+                linearLayout.setVisibility(View.INVISIBLE);
+                transaction.replace(R.id.main1_LinearLayout,new LoginFragment());
+                //返回上一页
+                transaction.addToBackStack(null);
+                transaction.commit();
+        }
+        return true;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         dbHelper=new MyDatabaseHelper(this,"BookStore.db",null,1);
         saveButton=findViewById(R.id.saveButton);
+        linearLayout=(LinearLayout) findViewById(R.id.main_LinearLayout);
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dbHelper.getWritableDatabase();
             }
         });
+
+
 
     }
 }
