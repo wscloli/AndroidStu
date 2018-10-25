af57e02899b6adad8ed3cc55188cf878b1374280
wscloli <2549271039@qq.com>
2018-10-25 17:44:15 +0800

    study

--- android2/Data Storage/DatabaseTest/app/src/main/java/com/example/administrator/databasetest/LoginFragment.java
+++ android2/Data Storage/DatabaseTest/app/src/main/java/com/example/administrator/databasetest/LoginFragment.java
@@ -1,5 +1,6 @@
 package com.example.administrator.databasetest;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,13 +9,18 @@ import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class LoginFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+       Context context=getActivity();
         //获取碎片布局
-        View view=inflater.inflate(R.layout.login_fragment, container,false);
+        View view=inflater.inflate(R.layout.login_fragment,container,false);
+        //View view=LayoutInflater.from(getActivity()).inflate(R.layout.login_fragment, null);
+        Button button=view.findViewById(R.id.button_Click);
         return view;
+
     }
 }
--- android2/Data Storage/DatabaseTest/app/src/main/java/com/example/administrator/databasetest/MainActivity.java
+++ android2/Data Storage/DatabaseTest/app/src/main/java/com/example/administrator/databasetest/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.administrator.databasetest;
 
 import android.app.FragmentManager;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,35 +17,37 @@ import android.support.v4.app.Fragment;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-private Button saveButton;
-private Button createButton;
-private CheckBox saveCheck;
-private EditText accountEdit;
-private EditText passEdit;
-private MyDatabaseHelper dbHelper;
-    LinearLayout linearLayout;
+    private Button saveButton;
+    private Button addButton;
 
-    public MainActivity() {
-    }
+    private Button createButton;
+    private CheckBox saveCheck;
+    private EditText accountEdit;
+    private EditText passEdit;
+    private MyDatabaseHelper dbHelper;
+    LinearLayout linearLayout;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main,menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-//定义菜单响应事件
+
+    //定义菜单响应事件
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.login_item:
-                android.support.v4.app.FragmentManager fragmentManager=getSupportFragmentManager();
-                FragmentTransaction transaction=fragmentManager.beginTransaction();
-
-                linearLayout.setVisibility(View.INVISIBLE);
-                transaction.replace(R.id.main1_LinearLayout,new LoginFragment());
+                Intent intent=new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+                /*android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+//INVISIBLE 隐藏（位置可见）     GONE 隐藏（不可见）     VISIBLE 可见
+                saveButton.setVisibility(View.GONE);
+                transaction.replace(R.id.main_LinearLayout, new LoginFragment());
                 //返回上一页
                 transaction.addToBackStack(null);
-                transaction.commit();
+                transaction.commit();*/
         }
         return true;
     }
@@ -51,17 +56,44 @@ private MyDatabaseHelper dbHelper;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        linearLayout = (LinearLayout) findViewById(R.id.main_LinearLayout);
 
-        dbHelper=new MyDatabaseHelper(this,"BookStore.db",null,1);
-        saveButton=findViewById(R.id.saveButton);
-        linearLayout=(LinearLayout) findViewById(R.id.main_LinearLayout);
+        dbHelper = new MyDatabaseHelper(this, "BookStore.db", null, 1);
+        //创建数据库
+        saveButton = findViewById(R.id.saveButton);
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dbHelper.getWritableDatabase();
             }
         });
-
+                //添加数据
+        addButton = (Button) findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //SQLiteOpenHelper 中的方法  创建和读取数据库
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                //存储类型，只能存基本类型，键值对
+                ContentValues values = new ContentValues();
+                //组装第一条数据
+                values.put("name", "C++");
+                values.put("author", "Wang");
+                values.put("pages", 454);
+                values.put("prices", 80);
+                //数据插入数据库中
+                db.insert("Book", null, values);
+                //清除数据
+                values.clear();
+                //组装第二条数据
+                values.put("name", "Java");
+                values.put("author", "liu");
+                values.put("pages", 1500);
+                values.put("prices", 90);
+                db.insert("Book", null, values);
+                values.clear();
+            }
+        });
 
 
     }
